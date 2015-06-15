bb8f077aade5dac61596d54cdd0487964cd0752a
mcvicker <danielmcvicker@gmail.com>
2015-06-15 10:02:38 -0600

    Update line breaks in tournament.sh
    Increases readability when deploying and testing project.

--- vagrant/tournament/tournament.sh
+++ vagrant/tournament/tournament.sh
@@ -1,9 +1,14 @@
 #!bin/bash
 echo "Creating or recreating tournament database..."
+echo " "
 psql -X -d vagrant -f tournament.sql
+echo " "
 echo "Tournament database created or recreated."
+echo " "
 echo "Running tournament_test.py..."
+echo " "
 python tournament_test.py
 echo "Tournament_test.py completed."
+echo " "
 echo "Tests complete."
 exit
